53f7c0fdb1675a3c94a03cda7b3832ba2a3cd144
Colby Johnson <cjohnsonspam@icloud.com>
2025-03-19 22:16:12 -0400

    HeeHee Haw Haw HE Haw

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-def AvoidanceProtocol():
-    global SR04
-    while True:
-        SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
-        if SR04 <= 15:
-            turn_right(0.5)
-            SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
-            if SR04 <= 15:
-                cuteBot.move_time(cuteBot.Direction.RIGHT, 15, 2)
-                basic.pause(100)
-                SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
-                if SR04 <= 15:
-                    cuteBot.move_time(cuteBot.Direction.RIGHT, 15, 2)
-                    basic.pause(100)
-                    SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
-                    if SR04 <= 15:
-                        cuteBot.motors(-15, 15)
-                        music.play_tone(262, music.beat(BeatFraction.WHOLE))
-                    else:
-                        cuteBot.motors(15, 15)
-                else:
-                    cuteBot.motors(15, 15)
-            else:
-                cuteBot.motors(151, 15)
-        else:
-            cuteBot.motors(15, 15)
-# Call the SonarVisualize function continuously
+# def AvoidanceProtocol():
+# global SR04
+# while True:
+# SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+# if SR04 <= 15:
+# turn_right(0.5)
+# SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+# if SR04 <= 15:
+# cuteBot.move_time(cuteBot.Direction.RIGHT, 15, 2)
+# basic.pause(100)
+# SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+# if SR04 <= 15:
+# cuteBot.move_time(cuteBot.Direction.RIGHT, 15, 2)
+# basic.pause(100)
+# SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+# if SR04 <= 15:
+# cuteBot.motors(-15, 15)
+# music.play_tone(262, music.beat(BeatFraction.WHOLE))
+# else:
+# cuteBot.motors(15, 15)
+# else:
+# cuteBot.motors(15, 15)
+# else:
+# cuteBot.motors(151, 15)
+# else:
+# cuteBot.motors(15, 15)
+# def attemptAuto():
+# global SR04
+# while True:
+# SR04 = cutebot.ultras
 def ServoToggle():
     global servostate
     if servostate == 0:
@@ -44,10 +47,6 @@ def Sonar():
     else:
         cuteBot.motors(25, 25)
         Sonar()
-def specialEd(times: number):
-    cuteBot.motors(50, 50)
-    basic.pause(1000 * times)
-    cuteBot.stopcar()
 
 def on_button_pressed_a():
     basic.clear_screen()
@@ -70,13 +69,20 @@ def WeeWoo():
     cuteBot.color_light(cuteBot.RGBLights.RGB_L, 0x0000ff)
     cuteBot.color_light(cuteBot.RGBLights.RGB_R, 0xff0000)
 
+def on_button_pressed_ab():
+    forwards(1)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 def on_button_pressed_b():
     cuteBot.set_servo(cuteBot.ServoList.S1, 0)
     cuteBot.set_servo(cuteBot.ServoList.S2, 0)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def forwards(times: number):
+    cuteBot.motors(50, 50.05)
+    basic.pause(500 * times)
+    cuteBot.stopcar()
 def turn_right(multiplier: number):
-    multiplier = 0
     cuteBot.stopcar()
     basic.pause(100)
     cuteBot.move_time(cuteBot.Direction.RIGHT, 50, 0.158 * multiplier)
@@ -104,24 +110,23 @@ def _90left(mmultiplier: number):
     cuteBot.stopcar()
     basic.pause(100)
     cuteBot.move_time(cuteBot.Direction.LEFT, 50, 0.158 * mmultiplier)
-multiplier2 = 0
 SR04 = 0
 servostate = 0
-multiplier22 = 0
-distance = 0
-multiplier23 = 0
 servostate = 0
-specialEd(1)
-basic.pause(1000)
-_90left(1)
-basic.pause(200)
-turn_right(3)
-basic.pause(200)
-specialEd(1)
-basic.pause(1000)
-_90left(1)
-basic.pause(200)
-specialEd(1)
+mmultiplier = 0
+multiplier = 0
+distance = 0
+# specialEd(1)
+# basic.pause(1000)
+# _90left(1)
+# basic.pause(200)
+# turn_right(3)
+# basic.pause(200)
+# specialEd(1)
+# basic.pause(1000)
+# _90left(1)
+# basic.pause(200)
+# specialEd(1)
 # def on_every_interval():
 # SonarVisual()
 # loops.every_interval(500, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,35 +1,33 @@
-function AvoidanceProtocol () {
-    while (true) {
-        SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-        if (SR04 <= 15) {
-            turn_right(0.5)
-            SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-            if (SR04 <= 15) {
-                cuteBot.moveTime(cuteBot.Direction.right, 15, 2)
-                basic.pause(100)
-                SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-                if (SR04 <= 15) {
-                    cuteBot.moveTime(cuteBot.Direction.right, 15, 2)
-                    basic.pause(100)
-                    SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-                    if (SR04 <= 15) {
-                        cuteBot.motors(-15, 15)
-                        music.playTone(262, music.beat(BeatFraction.Whole))
-                    } else {
-                        cuteBot.motors(15, 15)
-                    }
-                } else {
-                    cuteBot.motors(15, 15)
-                }
-            } else {
-                cuteBot.motors(151, 15)
-            }
-        } else {
-            cuteBot.motors(15, 15)
-        }
-    }
-}
-// Call the SonarVisualize function continuously
+// def AvoidanceProtocol():
+// global SR04
+// while True:
+// SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+// if SR04 <= 15:
+// turn_right(0.5)
+// SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+// if SR04 <= 15:
+// cuteBot.move_time(cuteBot.Direction.RIGHT, 15, 2)
+// basic.pause(100)
+// SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+// if SR04 <= 15:
+// cuteBot.move_time(cuteBot.Direction.RIGHT, 15, 2)
+// basic.pause(100)
+// SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.CENTIMETERS)
+// if SR04 <= 15:
+// cuteBot.motors(-15, 15)
+// music.play_tone(262, music.beat(BeatFraction.WHOLE))
+// else:
+// cuteBot.motors(15, 15)
+// else:
+// cuteBot.motors(15, 15)
+// else:
+// cuteBot.motors(151, 15)
+// else:
+// cuteBot.motors(15, 15)
+// def attemptAuto():
+// global SR04
+// while True:
+// SR04 = cutebot.ultras
 function ServoToggle () {
     if (servostate == 0) {
         servostate = 1
@@ -51,15 +49,25 @@ function Sonar () {
         Sonar()
     }
 }
-function specialEd (times: number) {
-    cuteBot.motors(50, 50)
-    basic.pause(1000 * times)
-    cuteBot.stopcar()
+function Nurse_to_Cardiac () {
+    forwards(0.5)
+    turn_right(0.85)
+    forwards(1.1)
+    turn_right(1.5)
 }
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    ServoToggle()
+	
 })
+function performPath (path: number) {
+    if (path == 3) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(1000)
+        basic.clearScreen()
+    } else {
+        cuteBot.stopcar()
+        basic.showIcon(IconNames.No)
+    }
+}
 function servoChange () {
     if (servostate == 0) {
         cuteBot.setServo(cuteBot.ServoList.S1, 0)
@@ -71,6 +79,9 @@ function servoChange () {
         basic.showNumber(1)
     }
 }
+function checkSonarNForward () {
+    SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+}
 function WeeWoo () {
     cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x0000ff)
     cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
@@ -78,15 +89,38 @@ function WeeWoo () {
     cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x0000ff)
     cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff0000)
 }
-input.onButtonPressed(Button.B, function () {
-    cuteBot.setServo(cuteBot.ServoList.S1, 0)
-    cuteBot.setServo(cuteBot.ServoList.S2, 0)
+input.onButtonPressed(Button.AB, function () {
+    Nurse_to_Cardiac()
+    Cardi_ER()
 })
+function Cardi_ER () {
+    forwards(0.4)
+    _90left(0.8)
+    forwards(1.15)
+    turn_right(1.5)
+}
+input.onButtonPressed(Button.B, function () {
+    Nurse_to_Cardiac()
+    cardi_room1()
+})
+function forwards (times: number) {
+    cuteBot.stopcar()
+    cuteBot.motors(50, 50.05)
+    basic.pause(777 * times)
+    cuteBot.stopcar()
+    basic.pause(100)
+}
+function cardi_room1 () {
+    forwards(1.3)
+    turn_right(0.85)
+    forwards(0.8)
+}
 function turn_right (multiplier: number) {
-    multiplier = 0
     cuteBot.stopcar()
     basic.pause(100)
     cuteBot.moveTime(cuteBot.Direction.right, 50, 0.158 * multiplier)
+    cuteBot.stopcar()
+    basic.pause(100)
 }
 function SonarVisual () {
     SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
@@ -115,25 +149,23 @@ function _90left (mmultiplier: number) {
     basic.pause(100)
     cuteBot.moveTime(cuteBot.Direction.left, 50, 0.158 * mmultiplier)
 }
-let multiplier = 0
 let SR04 = 0
 let servostate = 0
-let multiplier23 = 0
 let distance = 0
-let multiplier22 = 0
-let multiplier2 = 0
+let multiplier = 0
+let mmultiplier = 0
 servostate = 0
-specialEd(1)
-basic.pause(1000)
-_90left(1)
-basic.pause(200)
-turn_right(3)
-basic.pause(200)
-specialEd(1)
-basic.pause(1000)
-_90left(1)
-basic.pause(200)
-specialEd(1)
+// specialEd(1)
+// basic.pause(1000)
+// _90left(1)
+// basic.pause(200)
+// turn_right(3)
+// basic.pause(200)
+// specialEd(1)
+// basic.pause(1000)
+// _90left(1)
+// basic.pause(200)
+// specialEd(1)
 // def on_every_interval():
 // SonarVisual()
 // loops.every_interval(500, on_every_interval)
