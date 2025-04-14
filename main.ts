35d28bd42da3fb4d6fa1d7602abde078c0b9b524
Colby Johnson <cjohnsonspam@icloud.com>
2025-04-14 15:18:30 -0400

    Version Control

--- main.ts
+++ main.ts
@@ -5,19 +5,13 @@ function wiggle () {
 }
 function pickUpVial () {
     cuteBot.setServo(cuteBot.ServoList.S1, 120)
-    basic.pause(1000)
+    basic.pause(2000)
     cuteBot.setServo(cuteBot.ServoList.S1, 100)
-    basic.pause(200)
-    cuteBot.setServo(cuteBot.ServoList.S1, 90)
-    basic.pause(200)
-    cuteBot.setServo(cuteBot.ServoList.S1, 80)
-    basic.pause(200)
-    cuteBot.setServo(cuteBot.ServoList.S1, 70)
-    basic.pause(200)
+    basic.pause(500)
     cuteBot.setServo(cuteBot.ServoList.S1, 60)
-    basic.pause(200)
-    cuteBot.setServo(cuteBot.ServoList.S1, 30)
-    wiggle()
+    basic.pause(500)
+    cuteBot.setServo(cuteBot.ServoList.S1, 34)
+    cuteBot.setServo(cuteBot.ServoList.S2, 4)
 }
 function ServoToggle () {
     if (servostate == 0) {
@@ -27,24 +21,9 @@ function ServoToggle () {
     }
     servoChange()
 }
-function servoIncrease () {
-    let servoangle = 0
-    if (servoangle == 180) {
-    	
-    }
-}
-function Sonar () {
-    SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    basic.showNumber(SR04)
-    if (SR04 <= 20) {
-        cuteBot.stopcar()
-        basic.pause(1000)
-        cuteBot.motors(0, 20)
-        Sonar()
-    } else {
-        cuteBot.motors(25, 25)
-        Sonar()
-    }
+function servoStartUp () {
+    cuteBot.setServo(cuteBot.ServoList.S1, 0)
+    cuteBot.setServo(cuteBot.ServoList.S2, 45)
 }
 function Nurse_to_Cardiac () {
     forwards(0.5)
@@ -53,7 +32,7 @@ function Nurse_to_Cardiac () {
     turn_right(1.5)
 }
 input.onButtonPressed(Button.A, function () {
-    ServoToggle()
+	
 })
 function performPath (path: number) {
     if (path == 3) {
@@ -94,9 +73,6 @@ function servoChange () {
         basic.showNumber(1)
     }
 }
-function checkSonarNForward () {
-    SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-}
 function WeeWoo () {
     cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x0000ff)
     cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
@@ -105,8 +81,7 @@ function WeeWoo () {
     cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff0000)
 }
 input.onButtonPressed(Button.AB, function () {
-    Nurse_to_Cardiac()
-    Cardi_ER()
+	
 })
 function Cardi_ER () {
     forwards(0.4)
@@ -115,8 +90,7 @@ function Cardi_ER () {
     turn_right(1.5)
 }
 input.onButtonPressed(Button.B, function () {
-    Nurse_to_Cardiac()
-    cardi_room1()
+	
 })
 function forwards (times: number) {
     cuteBot.stopcar()
@@ -137,38 +111,15 @@ function turn_right (multiplier: number) {
     cuteBot.stopcar()
     basic.pause(100)
 }
-function SonarVisual () {
-    SR04 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    basic.clearScreen()
-    if (SR04 <= 5) {
-        led.plot(2, 4)
-    } else if (SR04 <= 10) {
-        led.plot(2, 3)
-    } else if (SR04 <= 15) {
-        led.plot(2, 2)
-    } else if (SR04 <= 20) {
-        led.plot(2, 1)
-    } else if (SR04 <= 25) {
-        led.plot(2, 0)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-    if (SR04 <= 10) {
-        led.plot(1, 4)
-        led.plot(3, 4)
-    }
-    basic.pause(100)
-}
 function _90left (mmultiplier: number) {
     cuteBot.stopcar()
     basic.pause(100)
     cuteBot.moveTime(cuteBot.Direction.left, 50, 0.158 * mmultiplier)
 }
-let SR04 = 0
 let servostate = 0
-let mmultiplier = 0
-let multiplier = 0
 let distance = 0
+let multiplier = 0
+let mmultiplier = 0
 servostate = 0
 servoChange()
 basic.forever(function () {
